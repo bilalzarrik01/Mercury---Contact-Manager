3dfd28c9f21878354519a4dcd2dfe9be8947e655
bilalzarrik01 <bilalzarrik11@gmail.com>
2026-01-29 10:30:10 +0100

    feat: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard', [
+        'groupCount' => \App\Models\Group::count(),
+        'contactCount' => \App\Models\Contact::count(),
+        'withPhoneCount' => \App\Models\Contact::whereNotNull('phone')->count(),
+    ]);
+})->name('dashboard');
+Route::resource('groups', GroupController::class)->except(['show']);
+Route::resource('contacts', ContactController::class)->except(['show']);
